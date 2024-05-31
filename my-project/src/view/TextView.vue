1ca1055251cbb000ba9ba411b679d61679535dac
mhlee <karyo13234@gmail.com>
2024-05-31 14:45:22 +0900

    v-for v-if 추가

--- my-project/src/view/TextView.vue
+++ my-project/src/view/TextView.vue
@@ -14,27 +14,62 @@
         <div>v-model text 예제 : {{ modelData }}</div>
         <div>4. vue v-bind 예제</div>
         <div v-bind:class="{ classTest: vBindClickFlg }">class 테스트</div>
+        <!-- 축약형 <div :class="{ classTest: vBindClickFlg }">class 테스트</div> -->
         <button v-on:click="chageClass">click me</button>
+        <div>5. vue v-if 예제</div>
+        <div v-if="showHide">if</div>
+        <div v-else-if="!showHide">else if</div>
+        <button @click="chageIf">click me</button>
+        <div>6. vue v-for 예제</div>
+        <ul>
+            <li v-for="user in userList" :key="user">
+                <div>postId : {{ user.postId }}</div>
+                <div>id : {{ user.id }}</div>
+                <div>name : {{ user.name }}</div>
+                <div>email : {{ user.email }}</div>
+                <div>body : {{ user.body }}</div>
+            </li>
+        </ul>
         <div>-------------------------------------------------</div>
-        </div>
+    </div>
 </template>
 
 <script>
 export default {
     created() {
-
+        this.userList = [
+            {
+                "postId": 1,
+                "id": 1,
+                "name": "id labore ex et quam laborum",
+                "email": "Eliseo@gardner.biz",
+                "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+            },
+            {
+                "postId": 1,
+                "id": 2,
+                "name": "quo vero reiciendis velit similique earum",
+                "email": "Jayne_Kuhic@sydney.com",
+                "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+            }
+        ];
     },
     data() {
         return {
             name: "Ask",
             clickData: false,
             modelData: "",
-            vBindClickFlg: false
+            vBindClickFlg: false,
+            showHide: false,
+            userList: [],
         }
     },
     methods: {
         chageText() {
             this.clickData = !this.clickData;
+        },
+        chageIf() {
+            this.showHide = !this.showHide;
         },
         chageClass() {
             this.vBindClickFlg = !this.vBindClickFlg;
