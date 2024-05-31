79e6dd59327f5c5b9db0c47952c6bf241e89d308
mhlee <karyo13234@gmail.com>
2024-05-31 14:03:52 +0900

    1.데이터 바인딩

---
+++ my-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <text-view></text-view>
+  </div>
+</template>
+
+<script>
+import TextView from './view/TextView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TextView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ my-project/src/view/TextView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div>-------------------------------------------------</div>
+        <div>1. vue 데이터 Data Binding</div>
+        {{ name }}
+        <div>2. vue v-on 예제</div>
+        <div>
+            {{ clickData }}
+        </div>
+        <button v-on:click="chageText">click me</button>
+        <!-- 축약형 <button @click="chageText">click me</button> -->
+        <div>3. vue v-model 예제</div>
+        <input type="text" v-model="modelData">
+        <div>v-model text 예제 : {{ modelData }}</div>
+        <div>4. vue v-bind 예제</div>
+        <div v-bind:class="{ classTest: vBindClickFlg }">class 테스트</div>
+        <button v-on:click="chageClass">click me</button>
+        <div>-------------------------------------------------</div>
+        </div>
+</template>
+
+<script>
+export default {
+    created() {
+
+    },
+    data() {
+        return {
+            name: "Ask",
+            clickData: false,
+            modelData: "",
+            vBindClickFlg: false
+        }
+    },
+    methods: {
+        chageText() {
+            this.clickData = !this.clickData;
+        },
+        chageClass() {
+            this.vBindClickFlg = !this.vBindClickFlg;
+        },
+    },
+}
+</script>
+
+<style scoped>
+.classTest {
+    color: blue;
+}
+</style>>
