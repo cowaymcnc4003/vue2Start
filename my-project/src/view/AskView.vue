7a7be695434fff4de4e3811ca6121e85e1670fcf
mhlee <karyo13234@gmail.com>
2024-06-04 19:47:26 +0900

    refactoringComponent

--- my-project/src/view/AskView.vue
+++ my-project/src/view/AskView.vue
@@ -1,95 +1,12 @@
 <template>
-  <div>
-    <ul class="news-list">
-      <li v-for="item in newsList" class="post" :key=item.id>
-        <!-- 포인트 영역-->
-        <div class="points">
-          {{ item.points || 0 }}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <template v-if="item.domain">
-              <a :href="item.url">
-                {{ item.title }}
-              </a>
-            </template>
-            <template v-else>
-              <router-link :to="`item/${item.id}`">
-                {{ item.title }}
-              </router-link>
-            </template>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link v-if="item.user" :to="`/user/${item.user}`" class="link-text">{{ item.user
-              }}</router-link>
-            <a :href="item.url" v-else>
-              {{ item.domain }}
-            </a>
-          </small>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <list-item></list-item>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import ListItem from '../components/ListItem.vue';
 export default {
-  computed: {
-    // 4번
-    // ...mapGetters(['fetchedUser'])
-    // 5번
-    ...mapGetters({
-      newsList: 'fetchedList'
-    })
-    // 3번
-    // ...mapState({
-    //     userList(state) {
-    //         return state.user
-    //     }
-    // })
-    // 2번
-    // userList() {
-    //     return this.$store.state.user;
-    // }
-  },
-  async created() {
-    this.$store.dispatch("FETCH_LIST", this.$route.name);
-    // let data = await fetchNewsList();
-    // console.log(data);
-  },
+  components: { ListItem },
 }
 </script>
 
-<style>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-
-.news-title {
-  margin: 0;
-}
-
-.link-text {
-  color: #828282;
-}
-</style>
+<style></style>
